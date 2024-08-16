2a10890a4a3eb47d5bf3e5f3e7624cd3fc3e7d6a
Lincoln Quick <lincolnmq@gmail.com>
2024-08-16 18:54:47 -0400

    Added comments and prompt for user

---
+++ product-quotient.py
@@ -0,0 +1,12 @@
+# CSC500 Module 1 Critical Thinking Assisgnment
+# Lincoln Quick
+# Part 2:
+# Write a Python program to find the multiplication and division of two numbers.
+
+# Ask the user to input two numbers (num1 and num2). 
+# Given those two numbers, multiply them together to find the output. 
+# Also, divide num1/num2 to find the output.
+
+# Ask the user to input two numbers
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
