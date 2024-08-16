99bf08fc6540a7e8f8a69b91f75507cec200e12d
Lincoln Quick <lincolnmq@gmail.com>
2024-08-16 18:55:47 -0400

    Added displaying the results

--- product-quotient.py
+++ product-quotient.py
@@ -15,3 +15,6 @@ num2 = int(input("Enter the second number: "))
 product = num1 * num2
 quotient = num1 / num2
 
+# Display the results
+print(f"The product of {num1} and {num2} is {product}")
+print(f"The quotient of {num1} and {num2} is {quotient}")
