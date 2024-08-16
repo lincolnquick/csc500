3e2e73c076f86d187752d016f445a386bac02282
Lincoln Quick <lincolnmq@gmail.com>
2024-08-16 18:49:04 -0400

    Changed 'user_num' to 'num' as specified in assignment

--- addition-subtraction.py
+++ addition-subtraction.py
@@ -1,18 +1,19 @@
 # CSC500 Module 1 Critical Thinking Assisgnment
+# Lincoln Quick
 # Part 1:
 # Write a Python program to find the addition and subtraction of two numbers.
 # Ask the user to input two numbers (num1 and num2). Given those two numbers, 
 # add them together to find the output. Also, subtract the two numbers to find the output.
 
 # Ask the user to input two numbers
-user_num1 = int(input("Enter the first number: "))
-user_num2 = int(input("Enter the second number: "))
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
 
 # Calculate the addition and subtraction of the two numbers
-addition = user_num1 + user_num2
-subtraction = user_num1 - user_num2
+addition = num1 + num2
+subtraction = num1 - num2
 
 # Display the results
-print(f"The sum of {user_num1} and {user_num2} is {addition}")
-print(f"The difference of {user_num1} and {user_num2} is {subtraction}")
+print(f"The sum of {num1} and {num2} is {addition}")
+print(f"The difference of {num1} and {num2} is {subtraction}")
 
