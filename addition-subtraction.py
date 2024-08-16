9036a4a977fcf10c4076f8d5fe28a47c4a29e04b
Lincoln Quick <lincolnmq@gmail.com>
2024-08-16 18:43:50 -0400

    Added prompts for user inputs

--- addition-subtraction.py
+++ addition-subtraction.py
@@ -8,10 +8,3 @@
 user_num1 = int(input("Enter the first number: "))
 user_num2 = int(input("Enter the second number: "))
 
-# Calculate the addition and subtraction of the two numbers
-addition = user_num1 + user_num2
-subtraction = user_num1 - user_num2
-
-# Display the results
-print(f"The sum of {user_num1} and {user_num2} is {addition}")
-print(f"The difference of {user_num1} and {user_num2} is {subtraction}")
