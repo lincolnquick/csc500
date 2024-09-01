961c9fd136e2f7efdd7d8cb26afc3c03e7e6bc4d
Lincoln Quick <lincolnmq@gmail.com>
2024-09-01 18:24:09 -0400

    Added prompt_user_for_item()

--- mod4/OnlineShoppingCart.py
+++ mod4/OnlineShoppingCart.py
@@ -1,3 +1,12 @@
 # Lincoln Quick
 # CSC 500, Module 4, Portfolio Milestone
 # 2024-09-01
+
+from ItemToPurchase import ItemToPurchase
+
+def prompt_user_for_item():
+    item = ItemToPurchase()
+    item.set_item_name(input('Enter the item name:\n'))
+    item.set_item_price(float(input('Enter the item price:\n')))
+    item.set_item_quantity(int(input('Enter the item quantity:\n')))
+    return item
