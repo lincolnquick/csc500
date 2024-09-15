e64e28db92085b6a817caad2b7347e8bc04d717e
Lincoln Quick <lincolnmq@gmail.com>
2024-09-15 00:57:29 -0400

    Prompt user for input of number of books purchased

---
+++ mod5/csc-bookstore.py
@@ -0,0 +1,16 @@
+# Lincoln Quick
+# CSC 500: Module 5 Critical Thinking Assignment Part 2
+# 2024-09-15
+# "The CSU Global Bookstore has a book club that awards points to its students based on the number of books 
+# purchased each month. The points are awarded as follows:
+# --If a customer purchases 0 books, they earn 0 points.
+# --If a customer purchases 2 books, they earn 5 points.
+# --If a customer purchases 4 books, they earn 15 points.
+# --If a customer purchases 6 books, they earn 30 points.
+# --If a customer purchases 8 or more books, they earn 60 points.
+# Write a program that asks the user to enter the number of books that they have purchased this month 
+# and then display the number of points awarded."
+
+# Get the number of books purchased
+books = int(input("Enter the number of books purchased this month: "))
+
