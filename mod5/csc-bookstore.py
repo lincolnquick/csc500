750163495744a98de6f8a1ba87d6559e24300e2c
Lincoln Quick <lincolnmq@gmail.com>
2024-09-15 01:00:46 -0400

    Calculate points earned for number of books purchased

--- mod5/csc-bookstore.py
+++ mod5/csc-bookstore.py
@@ -14,3 +14,14 @@
 # Get the number of books purchased
 books = int(input("Enter the number of books purchased this month: "))
 
+# Determine the number of points awarded
+if books <= 1:
+    points = 0  # If a customer purchases 0-1 books, they earn 0 points
+elif books <= 3:
+    points = 5  # If a customer purchases 2-3 books, they earn 5 points
+elif books <= 5:
+    points = 15  # If a customer purchases 4-5 books, they earn 15 points
+elif books <= 7:
+    points = 30  # If a customer purchases 6-7 books, they earn 30 points
+else:
+    points = 60 # If a customer purchases 8 or more books, they earn 60 points
